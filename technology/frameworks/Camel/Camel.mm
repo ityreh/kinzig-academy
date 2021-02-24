d7ba3390c6850d6d8df0b8b3a664211d8ac71588
Yannick Rehberger <yr@ityreh.de>
2021-02-24 20:52:00 +0100

    Add helm topics.

---
+++ technology/frameworks/Camel/Camel.mm
@@ -0,0 +1,21 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1613538410744" ID="ID_1765565102" MODIFIED="1613538417228" TEXT="Camel">
+<node CREATED="1613538419422" ID="ID_1611292768" MODIFIED="1613538422896" POSITION="right" TEXT="Camel K">
+<node CREATED="1613538424102" ID="ID_219755802" MODIFIED="1613538430039" TEXT="Knative">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://knative.dev/docs/
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1613539402779" ID="ID_1664885934" MODIFIED="1613539406162" TEXT="Modeline"/>
+</node>
+</node>
+</map>
--- technology/techniques/Blockchain/Blockchain.mm
+++ technology/techniques/Blockchain/Blockchain.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1612865923919" ID="ID_533123986" MODIFIED="1613499226073" TEXT="Blockchain">
+<node CREATED="1612865923919" ID="ID_533123986" MODIFIED="1614086129802" TEXT="Blockchain">
 <richcontent TYPE="NOTE"><html>
   <head>
     
