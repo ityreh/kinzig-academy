0c3ae0e50978824ff4c917f2d69eb5a38f1381ec
Yannick Rehberger <yr@ityreh.de>
2021-03-24 22:37:00 +0100

    Update

---
+++ technology/databases/MongoDB/MonogoDB.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1615620537006" ID="ID_1773073696" MODIFIED="1615620544429" TEXT="MonogoDB">
+<node CREATED="1615620608402" ID="ID_615328947" MODIFIED="1615620621165" POSITION="right" TEXT="ODM">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Object-Document Mapper
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1615620622480" ID="ID_589871824" MODIFIED="1616092195202" TEXT="Mongoid">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ODM for Ruby
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
--- technology/frameworks/Rails/Rails.mm
+++ technology/frameworks/Rails/Rails.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1615568833953" ID="ID_460269663" MODIFIED="1615568972189" TEXT="Rails">
+<node CREATED="1615568833953" ID="ID_460269663" MODIFIED="1615915914324" TEXT="Rails">
 <node CREATED="1615578986524" ID="ID_892138245" MODIFIED="1615583301224" POSITION="right" TEXT="generators">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -40,8 +40,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579594811" ID="ID_357689717" MODIFIED="1615579597942" POSITION="left" TEXT="app">
 <node CREATED="1615579630802" ID="ID_724805562" MODIFIED="1615579641502" TEXT="controllers"/>
@@ -63,8 +62,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">Contains the&#160;</font><code style="margin-top: 0; margin-bottom: 0; margin-right: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; border-top-style: none; border-top-width: 0px; border-right-style: none; border-right-width: 0px; border-bottom-style: none; border-bottom-width: 0px; border-left-style: none; border-left-width: 0px; font-size: 14px; background-color: rgb(255, 255, 255); background-image: null; background-repeat: repeat; background-attachment: scroll; background-position: null; font-family: Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace; line-height: 1.5; color: rgb(34, 34, 34); white-space: pre-wrap; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; word-spacing: 0px"><font size="14px" face="Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace" color="rgb(34, 34, 34)">rails</font></code><font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">&#160;script that starts your app and can contain other scripts you use to set up, update, deploy, or run your application.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579753942" ID="ID_847006184" MODIFIED="1615579755927" POSITION="left" TEXT="config">
 <node CREATED="1615580666608" ID="ID_1953609198" MODIFIED="1615582522940" TEXT="routes.rb">
@@ -98,8 +96,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">get &quot;/articles&quot;, to: &quot;articles#index&quot;</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1615579803051" ID="ID_1050077254" MODIFIED="1615579807179" POSITION="left" TEXT="config.ru"/>
@@ -113,8 +110,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">Contains your current database schema, as well as the database migrations.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579873555" ID="ID_1369591524" MODIFIED="1615579919744" POSITION="left" TEXT="Gemfile and Gemfile.lock">
 <richcontent TYPE="NOTE"><html>
@@ -126,8 +122,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">These files allow you to specify what gem dependencies are needed for your Rails application. These files are used by the Bundler gem. For more information about Bundler, see the&#160;</font><a href="https://bundler.io/" style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; border-top-style: none; border-top-width: 0px; border-right-style: none; border-right-width: 0px; border-bottom-style: none; border-bottom-width: 0px; border-left-style: none; border-left-width: 0px; font-size: 14px; background-color: rgb(255, 255, 255); background-image: null; background-repeat: repeat; background-attachment: scroll; background-position: null; color: rgb(152, 9, 5); text-decoration: underline; font-family: Helvetica, Arial, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font size="14px" color="rgb(152, 9, 5)" face="Helvetica, Arial, sans-serif"><u>Bundler website</u></font></a><font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif">.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579913701" ID="ID_1243758023" MODIFIED="1615579924211" POSITION="left" TEXT="lib">
 <richcontent TYPE="NOTE"><html>
@@ -139,8 +134,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">Extended modules for your application.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579949742" ID="ID_1370903721" MODIFIED="1615579958067" POSITION="left" TEXT="log">
 <richcontent TYPE="NOTE"><html>
@@ -152,8 +146,7 @@
       Application log files.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615579985234" ID="ID_1501419116" MODIFIED="1615579992427" POSITION="left" TEXT="package.json">
 <richcontent TYPE="NOTE"><html>
@@ -165,8 +158,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">This file allows you to specify what npm dependencies are needed for your Rails application. This file is used by Yarn. For more information about Yarn, see the&#160;</font><a href="https://yarnpkg.com/lang/en/" style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; border-top-style: none; border-top-width: 0px; border-right-style: none; border-right-width: 0px; border-bottom-style: none; border-bottom-width: 0px; border-left-style: none; border-left-width: 0px; font-size: 14px; background-color: rgb(255, 255, 255); background-image: null; background-repeat: repeat; background-attachment: scroll; background-position: null; color: rgb(152, 9, 5); text-decoration: underline; font-family: Helvetica, Arial, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font size="14px" color="rgb(152, 9, 5)" face="Helvetica, Arial, sans-serif"><u>Yarn website</u></font></a><font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615580005013" ID="ID_486003832" MODIFIED="1615580009273" POSITION="left" TEXT="public">
 <richcontent TYPE="NOTE"><html>
@@ -178,8 +170,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">Contains static files and compiled assets. When your app is running, this directory will be exposed as-is.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615580033580" ID="ID_1491669097" MODIFIED="1615580038174" POSITION="left" TEXT="Rakefile">
 <richcontent TYPE="NOTE"><html>
@@ -191,8 +182,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">This file locates and loads tasks that can be run from the command line. The task definitions are defined throughout the components of Rails. Rather than changing&#160;</font><code style="margin-top: 0; margin-bottom: 0; margin-right: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; border-top-style: none; border-top-width: 0px; border-right-style: none; border-right-width: 0px; border-bottom-style: none; border-bottom-width: 0px; border-left-style: none; border-left-width: 0px; font-size: 14px; background-color: rgb(255, 255, 255); background-image: null; background-repeat: repeat; background-attachment: scroll; background-position: null; font-family: Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace; line-height: 1.5; color: rgb(34, 34, 34); white-space: pre-wrap; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; word-spacing: 0px"><font size="14px" face="Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace" color="rgb(34, 34, 34)">Rakefile</font></code><font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">, you should add your own tasks by adding files to the&#160;</font><code style="margin-top: 0; margin-bottom: 0; margin-right: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; border-top-style: none; border-top-width: 0px; border-right-style: none; border-right-width: 0px; border-bottom-style: none; border-bottom-width: 0px; border-left-style: none; border-left-width: 0px; font-size: 14px; background-color: rgb(255, 255, 255); background-image: null; background-repeat: repeat; background-attachment: scroll; background-position: null; font-family: Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace; line-height: 1.5; color: rgb(34, 34, 34); white-space: pre-wrap; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; word-spacing: 0px"><font size="14px" face="Anonymous Pro, Inconsolata, Menlo, Consolas, Bitstream Vera Sans Mono, Courier New, monospace" color="rgb(34, 34, 34)">lib/tasks</font></code><font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">&#160;directory of your application.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615580067207" ID="ID_741880794" MODIFIED="1615580073091" POSITION="left" TEXT="README.md">
 <richcontent TYPE="NOTE"><html>
@@ -204,8 +194,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">This is a brief instruction manual for your application. You should edit this file to tell others what your application does, how to set it up, and so on.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615580094206" ID="ID_1525772796" MODIFIED="1615580095548" POSITION="left" TEXT="storage"/>
 <node CREATED="1615580095926" ID="ID_828564696" MODIFIED="1615580097504" POSITION="left" TEXT="test"/>
@@ -220,8 +209,7 @@
       <font color="rgb(51, 51, 51)" face="Helvetica, Arial, sans-serif" size="14px">A place for all third-party code. In a typical Rails application this includes vendored gems.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1615580143422" ID="ID_226869380" MODIFIED="1615580145907" POSITION="left" TEXT=".gitignore"/>
 <node CREATED="1615580146290" ID="ID_644987925" MODIFIED="1615580149392" POSITION="left" TEXT=".ruby-version"/>
@@ -247,8 +235,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1615583678355" ID="ID_1212101242" MODIFIED="1615583691198" TEXT="migrations">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -259,14 +246,13 @@
       bin/rails db:migrate
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node CREATED="1615582804790" ID="ID_1712194858" MODIFIED="1615582805931" POSITION="right" TEXT="view"/>
 <node CREATED="1615582806360" ID="ID_699214726" MODIFIED="1615582808047" POSITION="right" TEXT="controller"/>
-<node CREATED="1615583783091" ID="ID_1754089060" MODIFIED="1615583796494" POSITION="right" TEXT="console">
+<node CREATED="1615583783091" ID="ID_1754089060" MODIFIED="1615706724023" POSITION="right" TEXT="console">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -276,8 +262,36 @@
       bin/rails console
     </p>
   </body>
+</html></richcontent>
+</node>
+<node CREATED="1615915916953" ID="ID_519704633" MODIFIED="1615915932195" POSITION="right" TEXT="engine">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Engines can be considered miniature applications that provide functionality to their host applications.
+    </p>
+    <p>
+      A Rails application is actually just a &quot;supercharged&quot; engine, with the Rails::Application class inheriting a lot of its behavior from Rails::Engine.
+    </p>
+  </body>
 </html>
 </richcontent>
+<node CREATED="1615916258183" ID="ID_397489894" MODIFIED="1615916265185" TEXT="isolate namespace">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It is highly recommended that the isolate_namespace line be left within the Engine class definition. Without it, classes generated in an engine may conflict with an application.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </map>
